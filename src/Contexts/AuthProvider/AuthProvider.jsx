741a15c058721bbf4fdc79ebc62148762ff22823
tausifabid12 <tasifabid12@gmail.com>
2023-02-10 22:04:46 +0600

    auth provider commented

--- src/Contexts/AuthProvider/AuthProvider.jsx
+++ src/Contexts/AuthProvider/AuthProvider.jsx
@@ -18,7 +18,7 @@
 
 // export const AuthContext = createContext();
 
-const AuthProvider = ({ children }) => {
+// const AuthProvider = ({ children }) => {
 //   const [user, setUser] = useState(null);
 //   const [loading, setLoading] = useState(true);
 
